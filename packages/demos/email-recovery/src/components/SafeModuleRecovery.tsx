4de2fd8d77e1126c776c1771a4122b2c62ddc83f
Shubham Gupta <shubham.gupta@triomics.in>
2024-05-01 10:40:28 +0530

    :wip: Add embedded safe wallet

--- packages/demos/email-recovery/src/components/SafeModuleRecovery.tsx
+++ packages/demos/email-recovery/src/components/SafeModuleRecovery.tsx
@@ -7,11 +7,63 @@ import { useCallback, useContext, useEffect, useState } from "react";
 import { StepsContext } from "../App";
 import { STEPS } from "../constants";
 
+
+import { ethers } from "ethers";
+import { EthersAdapter } from "@safe-global/protocol-kit";
+
+import { SafeFactory } from "@safe-global/protocol-kit";
+
+
+const RPC_URL = "https://sepolia.base.org";
+const provider = new ethers.JsonRpcProvider(RPC_URL);
+
+const OWNER_1_PRIVATE_KEY=import.meta.env.VITE_OWNER_1_PRIVATE_KEY
+
+const owner1Signer = new ethers.Wallet(
+  OWNER_1_PRIVATE_KEY,
+  provider
+);
+
+const ethAdapterOwner1 = new EthersAdapter({
+  ethers,
+  signerOrProvider: owner1Signer,
+});
+
+
 const SafeModuleRecovery = () => {
-  const { address } = useAccount();
+  // const { address } = useAccount();
+  const address = "0x10f2743906D2C82D2240217D0203Bc4b5fD4b228"
   const { writeContractAsync } = useWriteContract();
   const stepsContext = useContext(StepsContext);
   const [loading, setLoading] = useState(false);
+  // const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    // creating burner safe. This will be created everytime the app is initialized
+    const createSafe = async () => {
+      const safeFactory = await SafeFactory.create({
+        ethAdapter: ethAdapterOwner1,
+        
+      });
+    
+      const safeAccountConfig = {
+        owners: [
+          await owner1Signer.getAddress(),
+        ],
+        threshold: 1,
+      };
+    
+      const protocolKitOwner1 = await safeFactory.deploySafe({ safeAccountConfig, saltNonce: Date.now().toString()});
+    
+      const safeAddress = await protocolKitOwner1.getAddress();
+    
+      console.log(`Safe address ${safeAddress}`);
+      // setAddress(safeAddress);
+    };
+    
+    // createSafe();
+        
+  }, [owner1Signer, ethAdapterOwner1])
 
   useEffect(() => {
     if (!address) {
